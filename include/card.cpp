4f482c6dc9475a0a139bfe79b0e01a11d08753f0
Jayden1161 <hnc4@sfu.ca>
2026-07-28 11:42:32 -0700

    Implement Card class with constructors and methods

--- include/card.cpp
+++ include/card.cpp
@@ -1,1 +1,33 @@
+#include "card.hpp"
 
+Card::Card()
+{
+    rank = Rank::TWO;
+    suit = Suit::NO_SUIT;
+}
+
+Card::Card(Rank newRank, Suit newSuit)
+{
+    rank = newRank;
+    suit = newSuit;
+}
+
+Card::~Card()
+{
+
+}
+
+Suit Card::getSuit() const
+{
+    return suit;
+}
+
+Rank Card::getRank() const
+{
+    return rank;
+}
+
+int Card::rankValue() const
+{
+    return static_cast<int>(rank);
+}
